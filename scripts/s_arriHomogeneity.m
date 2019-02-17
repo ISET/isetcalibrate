57266e64ed6b72a151128cd5f61913d910eba7b5
Brian Wandell <wandell@stanford.edu>
2019-02-17 12:41:02 -0800

    Small updates

--- scripts/s_arriHomogeneity.m
+++ scripts/s_arriHomogeneity.m
@@ -1,7 +1,10 @@
 %% Download and estimate homogeneity (linearity with respect to intensity)
 %
 % The SPD files contain the measured light spectra
-% The ARRI files contain the raw images (left and right) from the ARRI camera
+% The ARRI files contain the raw images (left and right) from the ARRI
+% camera 
+%
+%      set(groot,'defaultAxesColorOrder',co)
 %
 % BW/JEF  SCIENSTANFORD, 2019
 
@@ -9,32 +12,39 @@
 st = scitran('stanfordlabs');
 st.verify;
 
+% Find the project and the first calibration session
 project = st.lookup('arriscope/ARRIScope Calibration'); 
-
-% This is the first calibration session
 thisSession  = project.sessions.findOne('label="20190208"');
 
 % Working directory
 chdir(fullfile(icalRootPath,'local'));
 
-%% Get data for one of the channels
-channel = 'Red';   % 'Green','Blue','Violet','White', 'Infrared'
+%% Get data from an acquisition for one of the channels
+
+channel = 'Green';   % 'Red','Green','Blue','Violet','White', 'Infrared'
 str     = sprintf('label=%s',channel);
 Acquisition = thisSession.acquisitions.findOne(str);
-
-%{
-zipInfo = Acquisition.getFileZipInfo(zipFile);
-thisZip{1} = zipInfo; stPrint(thisZip,'members','path')
-%}
 
 %%  Down load the spectra.  Not very big.
 
 spdZipFile = sprintf('%s_LightSpectra_mat.zip',channel);
 spdFile = Acquisition.getFile(spdZipFile);
 spdZip = sprintf('%s_spd.zip',channel);
-spdFile.download(spdZip)
+spdFile.download(spdZip);
 spdDir = sprintf('%s_spd',channel);
 unzip(spdZip,spdDir);
+
+
+%{
+% Programming note
+% If you want to see the files inside a ZIP file, you can do this
+zipInfo    = Acquisition.getFileZipInfo(zipFile);
+thisZip{1} = zipInfo; stPrint(thisZip,'members','path')
+
+entryName = zipInfo.members{1}.path;
+outPath = fullfile('/tmp', entryName);
+acquisition.downloadFileZipMember('my-archive.zip', entryName, outPath);
+%}
 
 %% Download the arri images.  These are pretty big
 
@@ -93,7 +103,7 @@ levels = levels/max(levels(:));
 ieNewGraphWin;
 plot(code/max(code),levels,'o')
 grid on; xlabel('Scaled file code'); ylabel('SPD level');
-set(gca,'ylim',[0 1.1]);
+set(gca,'ylim',[0 1.1]); legend({'R','G','B'});
 identityLine;
 
 %% Find the mean values in the region of the ARRI images
@@ -117,12 +127,12 @@ for ii=1:nFiles
     arriMean(:,ii) = mean(RGB2XWFormat(arriCrop))';
 end
 
-% Figure out how to plot the color order correctly.
+% 
 ieNewGraphWin;
 plot(code,arriMean,'o');
 set(gca,'xlim',[0 10]);
 xlabel('Code level'); ylabel('Channel mean');
-grid on; 
+grid on; legend({'R','G','B'});
 
 %{
   [~,rect] = imcrop(rgb);
