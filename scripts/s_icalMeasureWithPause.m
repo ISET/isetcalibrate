0c6e3ae816938f21de982dbe64956bc3b06c4375
ZhengLyuFelix <zhenglyu@stanford.edu>
2019-12-19 15:08:09 -0800

    Simplified for single measurement

--- scripts/s_icalMeasureWithPause.m
+++ scripts/s_icalMeasureWithPause.m
@@ -1,4 +1,4 @@
-%% s_iCalMeasureWithPuase - based on ZLY script
+%% s_iCalMeasureWithPause
 %
 % Remotely control the PR670 photospectrometer to measure spectral
 % reflection for OralEye project. Edit the code so that the result can be
@@ -8,42 +8,24 @@
 % the IOPort directories.
 %
 
+%{
+% Remember that for the PsychToolbox this must be the ordering of the
+% IOPort call.
+
+which -all IOPort
+C:\Users\SCIENlab\Documents\MATLAB\Psychtoolbox\Psychtoolbox-3-master\Psychtoolbox\PsychBasic\MatlabWindowsFilesR2007a\IOPort.mexw64
+C:\Users\SCIENlab\Documents\MATLAB\Psychtoolbox\Psychtoolbox-3-master\Psychtoolbox\PsychBasic\IOPort.m   
+%}
+
 %% Initialize isetcam to use ieSaveSpectralFiles.m function
 ieInit;
 
-%%
-tic
-% Repeated measurements of a light
-pathName = '20191218';
-folderName = fullfile(icalRootPath, 'local', pathName);
-if ~exist(folderName, 'dir'), mkdir(folderName); end
-cd(folderName);
-% SubjectName = 'WhiteTarget_Velscope';
-%% Define the filename session
+%% Initialize the PR670 with PsychToolbox functions
 
-target = 'whiteSurface'; % tongue or white target
-lightsource = 'OralEye2_BP'; % LED400 / LED425 / LED450
-filter = 'None'; % Y52 / NoY52
-posNumber = 'position1'; % # of the positions
-SubjectName = strcat(target,'_' ,lightsource,'_' ,filter,'_' ,posNumber);
-
-%% Define the comment
-subjectNumber = 'none';
-shortPassFilter = 'None';
-spectroRadioMeterModel = 'PR670';
-longPassFilter = 'None';
-power = 'None';
-apertureSize = '1';
-
-comment = strcat(target, " measurements for subject ", subjectNumber,...
-            " illuminated with a ", lightsource, " with ", shortPassFilter,...
-            " shortpass filter and measured with a ", spectroRadioMeterModel,...
-            "spectroradiometer with ", longPassFilter,...
-            ". Power supply was: ", power, " watts.", ...
-            " The aperture size was: ", apertureSize, " deg.");
-%%
-photometerCOM = 'COM5';           % Select the correct COM port number for the photometer
-%spectr = figure;
+% Select the correct COM port number for the photometer
+% YOu can see which COM devices exist in the device manager under Ports
+% (COM & LPT) sections.
+photometerCOM = 'COM5';
 
 if ~isempty(instrfind)
     fclose(instrfind);
@@ -53,55 +35,80 @@ instrfind;
 
 % ph = PR670init(photometerCOM);
 msg = PR670init(photometerCOM);
+
+startW = 380;
+stepW  = 5;
+endW   = 780;
+wave   = startW:stepW:endW;  % Default pr670 wavelength sampling
+
+% This is the isetcalibrate function that doesn't seem to run right.  The
+% content seems to be for the 715 not the 670.
 % ph = pr670init(photometerCOM);
 % Change the pr aperture size to 0.5c
 % The PR will integrate the spectrum over a smaller region, but the
 % measurement will take longer
 % fprintf(ph,'S,,,1\n');
 % pause(1);
-%%
-nRepetitions = 3;
-data = [];
-%%
-wav = [350:5:780];
 
-for ii = 1 %:nRepetitions
-    fprintf('Measuring spectrum.... ');
-    spd =[];
+%% Initialize writing
+dirName = datestr(now,'yyyy-mm-dd');
+folderName = fullfile(icalRootPath, 'local', dirName);
+if ~exist(folderName, 'dir'), mkdir(folderName); end
+cd(folderName);
 
-    setwav = [350 3 87];
-    while isempty(spd)
-        spd = PR670measspd();
-        %     [spd, wav] = pr670spectrum(ph);
+%% Define the properties of this measurement
+
+info.target      = 'whiteSurface';  % tongue or white target
+info.lightsource = 'Tungsten'; % 
+info.filter      = 'NIR';      % NIR
+info.apertureSize = '1';
+info.spectroRadioMeterModel = 'PR670';
+info.nRepetitions = 1;
+info.comment = 'Null';
+comment = jsonwrite(info);
+
+%% Make several measurements and save them
+
+spd  = zeros(length(wave),nRepetitions);
+for ii = 1:info.nRepetitions
+    fprintf('Measuring spectrum (%d).... ',ii);
+    % PsychToolbox function
+    spd(:,ii) = PR670measspd([startW stepW length(wave)]);
+    if ~isequal(length(wave),length(spd))
+        warning('wav sampling not equal to spd measurements');
     end
-%     figure();
-    hold on
-    plot(wav,spd);hold on
-
-    %save(sprintf('spectr%d.mat',80),'spd');
-    %cd('C:\Users\SCIENlab\Documents\MATLAB-scien\MATLAB\workdir\velscope')
-%     filename= sprintf([SubjectName,'_%dV.mat'],29-ii);
-    data = [data,spd];
-%     save(filename,'result');
-    fprintf('Done!\n');
-%     if ii~=3
-%         uiwait(msgbox('Please decrease the light level by 1 V.','Pause','modal'));
-%     end
+    fprintf('\n');
 end
 
-fullPathName = fullfile(pwd, SubjectName);
-saveas(gcf,[SubjectName,'.fig'],'fig');
-ieSaveSpectralFile(wav', data, comment, fullPathName);
-toc
-% fclose(ph);
+% isetcalibrate - not yet working we think
+%     [spd, wav] = pr670spectrum(ph);
+    
 %{
-s_400 = load('stanford_670_400.mat');
-s_400 = s_400.result;filename= sprintf('oraleye_2_20190711_oraleye_reflect_blue_10.mat');
-
-plot(s_400(1,:), s_400(2,:));hold on
-p_400 = load('715_400.mat');
-p_400 = p_400.result;
-plot(p_400(1,:), p_400(2,:));
+    ieNewGraphWin;
+    hold on
+    plot(wave,spd); xlabel('Wavelength (nm)'); 
+    ylabel('Radiance (watts/sr/m2/nm)')
 %}
-%% quit remote mode
+
+%% Save spectral data
+
+fname = sprintf('%s_%s_%s',info.target,info.lightsource,info.filter);
+fullPathName = fullfile(folderName, fname);
+fname =ieSaveSpectralFile(wave', spd, comment, fullPathName);
+
+% {
+nir = 'whiteSurface_Tungsten_NIR.mat';
+none = 'whiteSurface_Tungsten_none.mat';
+spdNIR = ieReadSpectra(nir,wave);
+spdNone = ieReadSpectra(none,wave);
+ieNewGraphWin; plot(wave,spdNIR,'k-',wave,spdNone,'r:');
+ieNewGraphWin; semilogy(wave,spdNIR ./ spdNone);
+grid on
+
+
+
+%}
+%% Quit PR670 remote mode
 PR670write('Q', 0);
+
+%% END
