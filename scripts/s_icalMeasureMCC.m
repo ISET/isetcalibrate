d3aa02d962630c5c497e2a572329e81103545ba2
ZhengLyuFelix <zhenglyu@stanford.edu>
2019-12-19 17:05:57 -0800

    Used for MCC measurements

--- scripts/s_icalMeasureMCC.m
+++ scripts/s_icalMeasureMCC.m
@@ -1,24 +1,14 @@
-%% s_iCalMeasureWithPause
+%% s_iCalMeasureMCC
 %
-% Remotely control the PR670 photospectrometer to measure spectral
-% reflection for OralEye project. Edit the code so that the result can be
-% saved with the same format as ieSaveSpectralFiles do.
 %
-% PsychToolbox must be on the path and it must have the proper ordering of
-% the IOPort directories.
-%
-%{
-chdir('C:\Users\SCIENlab\Documents\MATLAB\Psychtoolbox\Psychtoolbox-3-master\Psychtoolbox');
-SetupPsychtoolbox
-PsychStartup
-%}
+
 %{
 % Remember that for the PsychToolbox this must be the ordering of the
 % IOPort call.
 
 which -all IOPort
 C:\Users\SCIENlab\Documents\MATLAB\Psychtoolbox\Psychtoolbox-3-master\Psychtoolbox\PsychBasic\MatlabWindowsFilesR2007a\IOPort.mexw64
-C:\Users\SCIENlab\Documents\MATLAB\Psychtoolbox\Psychtoolbox-3-master\Psychtoolbox\PsychBasic\IOPort.m   
+C:\Users\SCIENlab\Documents\MATLAB\Psychtoolbox\Psychtoolbox-3-master\Psychtoolbox\PsychBasic\IOPort.m
 %}
 
 %% Initialize isetcam to use ieSaveSpectralFiles.m function
@@ -35,6 +25,9 @@ if ~isempty(instrfind)
     fclose(instrfind);
     delete(instrfind);
 end
+instrfind;
+
+% ph = PR670init(photometerCOM);
 msg = PR670init(photometerCOM);
 
 startW = 380;
@@ -52,42 +45,49 @@ wave   = startW:stepW:endW;  % Default pr670 wavelength sampling
 % pause(1);
 
 %% Initialize writing
-dirName = datestr(now,'yyyy-mm-dd');
+dirName = [datestr(now,'yyyy-mm-dd'),'-MCC'];
 folderName = fullfile(icalRootPath, 'local', dirName);
 if ~exist(folderName, 'dir'), mkdir(folderName); end
 cd(folderName);
 
 %% Define the properties of this measurement
-
-info.target      = 'whiteSurface';  % tongue or white target
-info.lightsource = 'Tungsten'; % 
-info.filter      = 'NIR';      % NIR
+info.target      = 'mcc patches';  % tongue or white target
+info.lightsource = 'Blue'; %
+info.filter      = 'Y44';      % NIR
 info.apertureSize = '1';
 info.spectroRadioMeterModel = 'PR670';
 info.nRepetitions = 1;
 info.comment = 'Null';
 comment = jsonwrite(info);
 
-%% Make several measurements and save them
+%%
+% {'brown','orange','blue','white'}
+oeSet('light','blue')
+ieNewGraphWin;
+nPatches = 24;
+spd  = zeros(length(wave),nPatches);
 
-spd  = zeros(length(wave),nRepetitions);
-for ii = 1:info.nRepetitions
+for pp = 1:24
+    fprintf('Ready for patch %d ',pp)
+    pause;
+    
+    %% Make measurements and save them
+
     fprintf('Measuring spectrum (%d).... ',ii);
     % PsychToolbox function
-    spd(:,ii) = PR670measspd([startW stepW length(wave)]);
+    spd(:,pp) = PR670measspd([startW stepW length(wave)]);
+    plot(wave,spd(:,pp));
+
     if ~isequal(length(wave),length(spd))
         warning('wav sampling not equal to spd measurements');
     end
     fprintf('\n');
+    
 end
 
-% isetcalibrate - not yet working we think
-%     [spd, wav] = pr670spectrum(ph);
-    
 %{
     ieNewGraphWin;
-    hold on
-    plot(wave,spd); xlabel('Wavelength (nm)'); 
+    plot(wave,spd); xlabel('Wavelength (nm)');
     ylabel('Radiance (watts/sr/m2/nm)')
 %}
 
@@ -97,18 +97,27 @@ fname = sprintf('%s_%s_%s',info.target,info.lightsource,info.filter);
 fullPathName = fullfile(folderName, fname);
 fname =ieSaveSpectralFile(wave', spd, comment, fullPathName);
 
-% {
-nir = 'whiteSurface_Tungsten_NIR.mat';
-none = 'whiteSurface_Tungsten_none.mat';
-spdNIR = ieReadSpectra(nir,wave);
-spdNone = ieReadSpectra(none,wave);
-ieNewGraphWin; plot(wave,spdNIR,'k-',wave,spdNone,'r:');
-ieNewGraphWin; semilogy(wave,spdNIR ./ spdNone);
-grid on
+%%
+actions = oeActions('file name','mccWhite',...
+    'directory',folderName,...
+    'subject code','MCC',...
+    'light',{'white','white'}, ...
+    'exposure time',[30000 30000]);
+ 
+files = oeCapture(actions);
+oeShow(files{1})
 
+%%
+actions = oeActions('file name','mccBlue',...
+    'directory',folderName,...
+    'subject code','MCC',...
+    'light',{'blue','blue'}, ...
+    'exposure time',[10000,10000]);
 
+files = oeCapture(actions);
+oeShow(files{1})
+oeShow(files{2})
 
-%}
 %% Quit PR670 remote mode
 PR670write('Q', 0);
 
