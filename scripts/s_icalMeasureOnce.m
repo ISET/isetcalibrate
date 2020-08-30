eeea20160198594081994a1bf4c5844e23362e0c
Brian Wandell <wandell@stanford.edu>
2020-08-29 18:43:38 -0700

    Window interface for controlling the PR 670

--- scripts/s_icalMeasureOnce.m
+++ scripts/s_icalMeasureOnce.m
@@ -1,4 +1,4 @@
-%% s_iCalMeasureMCC
+%% s_iCalMeasureOnce
 %
 %
 
@@ -9,9 +9,12 @@
 which -all IOPort
 C:\Users\SCIENlab\Documents\MATLAB\Psychtoolbox\Psychtoolbox-3-master\Psychtoolbox\PsychBasic\MatlabWindowsFilesR2007a\IOPort.mexw64
 C:\Users\SCIENlab\Documents\MATLAB\Psychtoolbox\Psychtoolbox-3-master\Psychtoolbox\PsychBasic\IOPort.m
+
 If this is not the order, then we do this:
+
 cd C:\Users\SCIENlab\Documents\MATLAB\Psychtoolbox\Psychtoolbox-3-master\Psychtoolbox
-then run:
+SetupPsychtoolbox
+
 type yes to all the questions
 It should work after it finishes setup.
 %}
@@ -30,7 +33,7 @@ if ~isempty(instrfind)
     fclose(instrfind);
     delete(instrfind);
 end
-instrfind;
+tmp = instrfind;
 
 % ph = PR670init(photometerCOM);
 msg = PR670init(photometerCOM);
@@ -50,7 +53,7 @@ wave   = startW:stepW:endW;  % Default pr670 wavelength sampling
 % pause(1);
 
 %% Initialize writing
-dirName = [datestr(now,'yyyy-mm-dd'),'Tongue_Krithin_405nm'];
+dirName = [datestr(now,'yyyy-mm-dd'),'Calibrate-filters'];
 folderName = fullfile(icalRootPath, 'local', dirName);
 if ~exist(folderName, 'dir'), mkdir(folderName); end
 cd(folderName);
@@ -58,12 +61,12 @@ cd(folderName);
 %% Define the properties of this measurement
 % info.target      = 'white_surface';  % tongue or white target
 info.target      = 'White';
-info.lightsource = '405nm'; %
-info.filter      = 'SP_LP_Y44';      % 
+info.lightsource = 'tungstenLamp'; %
+info.filter      = 'Nofilter_and_PR670_Clear';      % 
 info.apertureSize = '1';
 info.spectroRadioMeterModel = 'PR670';
 info.nRepetitions = 1;
-info.comment = 'White_Illuminated_with_405_nm_and_SP_LP_Y44_apetertureSize_1';
+info.comment = 'White surface Illuminated with tungsten lamp apertureSize 1.  Determining the filter';
 comment = jsonwrite(info);
 
 %% Make measurements and save them
@@ -80,6 +83,8 @@ ieNewGraphWin;
 plot(wave,spd);
 xlabel('Wavelength (nm)');
 ylabel('Radiance (watts/sr/m2/nm)')
+set(gca,'yscale','log')
+grid on
 
 fname = sprintf('%s_%s_%s_%s',datestr(now,'hh-mm-ss'),info.target,info.lightsource,info.filter);
 fullPathName = fullfile(folderName, fname);
